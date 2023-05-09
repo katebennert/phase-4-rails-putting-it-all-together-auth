a2717a1b3f0f42c7cdedc985c2e521a4bd79cac1
Kate Bennert <kate.bennert@gmail.com>
2023-05-09 15:30:57 -0400

    commit tree 138b942def6a47018852f34f5801bc491672ec99

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,19 @@
+class RecipesController < ApplicationController
+
+  def index
+      recipes = Recipe.all
+      render json: recipes, include: :user, status: :ok
+  end  
+  
+    def create
+      user = User.find_by(id: session[:user_id])
+      recipe = user.recipes.create!(recipe_params)
+      render json: recipe, include: :user, status: :created
+    end
+
+    private
+
+    def recipe_params
+      params.permit(:title, :instructions, :minutes_to_complete)
+    end
+end
